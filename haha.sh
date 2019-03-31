8440f205df353f360327d403e76fd03111570444
Tyler Sehr <tysehr37@gmail.com>
2019-03-30 22:03:34 -0700

    Update haha.sh

--- haha.sh
+++ haha.sh
@@ -1,3 +1,4 @@
+open https://youtu.be/A_MjCqQoLLA?t=239
 while : 
 do 
 	osascript -e "tell application \"System Events\"" -e "key code 28 using {control down, option down, command down}" -e "end tell" 
