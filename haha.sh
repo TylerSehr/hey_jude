cb12ed4dc894afacf4769885e01940303a25addd
Tyler Sehr <tysehr37@gmail.com>
2019-09-15 20:19:09 -0700

    hatred is the only good emotion

--- haha.sh
+++ haha.sh
@@ -1,6 +1,7 @@
 osascript -e "set Volume 10"
 while : 
 do 
+	defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'; killall Dock
 	open "https://youtu.be/A_MjCqQoLLA?t=239"
 	osascript -e "tell application \"System Events\"" -e "key code 123 using {control down}" -e "end tell" 
 	osascript -e "tell application \"System Events\"" -e "key code 126 using {control down}" -e "end tell" 
